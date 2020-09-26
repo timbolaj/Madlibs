aa610ab93c3818433633c4facc07d423870c6575
Aaron <timbolaj@gmail.com>
2020-09-26 16:30:28 -0400

    remodeled the state object to conform to state changeS

--- src/app/form.reducer.ts
+++ src/app/form.reducer.ts
@@ -2,31 +2,21 @@ import { createReducer, on } from '@ngrx/store';
 import { reset, save } from './form.actions';
 
 export interface State {
-  name: string,
-  adjective1: string,
-  adjective2: string,
-  adverb: string,
-  city: string,
-  nameOfFriend: string,
+  form: {}
 }
 
 export const initialState: State = {
-  name: '',
-  adjective1: '',
-  adjective2: '',
-  adverb: '',
-  city: '',
-  nameOfFriend: '',
+  form: {}
 }
 
 export const _formReducer = createReducer(
   initialState,
 
-  on(reset, state => initialState),
+  on(reset, () => initialState),
   on(save, (state, form) => (
     {
+    ...state,
     form,
-    ...state
   }))
 )
 
